a48603ec7d9f999bd1bb028e847b259faf55edd4
Jonathan Warykowski <jonathan@myzeller.com>
2024-02-08 23:50:35 +1100

    chore: updated nvim-treesitter configuration

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,10 @@
 return {
-  -- { import = "lazyvim.plugins.extras.lang.json" },
-  -- { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
 
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- extends default config
       vim.list_extend(opts.ensure_installed, {
         "tsx",
         "typescript",

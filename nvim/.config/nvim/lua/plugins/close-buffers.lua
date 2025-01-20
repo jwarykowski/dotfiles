d776b9adb408e7ffffc154caff34eadb18339b8b
Jonathan Warykowski <jonathan@myzeller.com>
2025-01-20 20:42:48 +1100

    chore: tweaked close-buffers configuration

--- nvim/.config/nvim/lua/plugins/close-buffers.lua
+++ nvim/.config/nvim/lua/plugins/close-buffers.lua
@@ -1,12 +1,8 @@
-local keymap = function()
-	vim.keymap.set("n", "<leader>Q", ":Bdelete menu<cr>")
-end
-
 return {
 	{
 		"asheq/close-buffers.vim",
 		config = function()
-			keymap()
+			vim.keymap.set("n", "<leader>Q", ":Bdelete menu<cr>")
 		end,
 	},
 }

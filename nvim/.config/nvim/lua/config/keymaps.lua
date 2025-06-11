0f7b3357aa73f43f1451c5e78856f5f6196e72e3
Jonathan Warykowski <jonathan@myzeller.com>
2025-06-12 09:51:30 +1000

    chore: removed keymaps not used

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -29,26 +29,6 @@ map("n", "dw", "vb_d")
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- move lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
-
--- move up/down
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-
--- resize window
--- map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "increase window width" })
--- map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "decrease window width" })
--- map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "decrease window height" })
--- map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "increase window height" })
-
 -- move to window
 map("n", "<C-h>", "<C-w>h", { desc = "move to left window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "move to right window", remap = true })

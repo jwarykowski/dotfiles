8367526326571bcd64f907bfd4605a93f48b447e
Jonathan Warykowski <jonathan@myzeller.com>
2025-06-12 09:56:19 +1000

    chore: updated lsp configuration
    * added ominisharp
    * updated on attach

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,15 +15,17 @@ return {
 				"html",
 				"jsonls",
 				"lua_ls",
+				"omnisharp",
 				"pyright",
 				"rust_analyzer",
-				"ts_ls",
 				"tailwindcss",
+				"ts_ls",
 				"yamlls",
 			},
 			automatic_enable = false,
 		},
 	},
+
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = { "saghen/blink.cmp" },
@@ -33,13 +35,60 @@ return {
 			local mason_lspconfig = require("mason-lspconfig")
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			-- local function on_attach(client, bufnr) end
+			local function on_attach(client, bufnr)
+				local kmap = function(mode, keys, func, desc)
+					if desc then
+						desc = "LSP: " .. desc
+					end
+					vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
+				end
+
+				-- lsp mappings
+
+				-- buf
+				kmap("n", "gd", vim.lsp.buf.definition, "go to definition")
+				kmap("n", "grr", vim.lsp.buf.references, "references")
+				kmap("n", "gD", vim.lsp.buf.declaration, "go to declaration")
+				kmap("n", "gri", vim.lsp.buf.implementation, "go to implementation")
+
+				-- hover
+				kmap("n", "K", vim.lsp.buf.hover, "hover documentation")
+				kmap("n", "<leader>k", vim.lsp.buf.signature_help, "signature help")
+				kmap("i", "<C-s>", vim.lsp.buf.signature_help, "signature help")
+
+				-- actions
+				kmap("n", "grn", vim.lsp.buf.rename, "rename")
+				kmap("n", "gra", vim.lsp.buf.code_action, "code action")
+				kmap("n", "gO", vim.lsp.buf.document_symbol, "document symbol")
+
+				-- diagnostics
+				kmap("n", "gl", vim.diagnostic.open_float, "line diagnostics")
+				-- kmap("n", "[d", vim.diagnostic.goto_prev, "prev diagnostic")
+				-- kmap("n", "]d", vim.diagnostic.goto_next, "next diagnostic")
+			end
+
+			lspconfig.omnisharp.setup({
+				cmd = {
+					vim.fn.expand("~/.local/share/nvim/mason/packages/omnisharp/omnisharp"),
+				},
+				on_attach = on_attach,
+				capabilities = capabilities,
+				enable_roslyn_analyzers = true,
+				enable_editorconfig_support = true,
+				enable_import_completion = true,
+				organize_imports_on_format = true,
+				sdk_include_prereleases = true,
+				analyze_open_documents_only = false,
+				root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", ".git"),
+			})
 
 			for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
-				lspconfig[server_name].setup({
-					-- on_attach = on_attach,
-					capabilities = capabilities,
-				})
+				if server_name ~= "omnisharp" then
+					lspconfig[server_name].setup({
+						on_attach = on_attach,
+						capabilities = capabilities,
+					})
+				end
 			end
 		end,
 		opts = function(_, opts)
@@ -47,6 +96,8 @@ return {
 				float = {
 					border = "rounded",
 				},
+				severity_sort = true,
+				update_in_insert = false,
 			}
 		end,
 	},

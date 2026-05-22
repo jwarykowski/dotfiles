8438e8d5237e4c3273a61ff70b6463e1167cdc0d
Jonathan Warykowski <jonathan@myzeller.com>
2026-05-22 23:03:25 +1000

    fix: updated plugins and nvim treesitter configuration

--- common/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ common/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,67 +1,31 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			modules = {},
-			auto_install = true,
-			ignore_install = {},
-			sync_install = false,
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"bash",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"json",
-				"python",
-				"go",
-				"rust",
-				"markdown",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-				},
-			},
-		})
+		local ts = require("nvim-treesitter")
+		ts.setup()
+
+		local ensure_installed = {
+			"bash",
+			"javascript",
+			"typescript",
+			"html",
+			"css",
+			"json",
+			"python",
+			"go",
+			"rust",
+		}
+
+		local installed = ts.get_available()
+
+		local missing = vim.tbl_filter(function(parser)
+			return not vim.tbl_contains(installed, parser)
+		end, ensure_installed)
+
+		if #missing > 0 then
+			ts.install(missing)
+		end
 	end,
 }

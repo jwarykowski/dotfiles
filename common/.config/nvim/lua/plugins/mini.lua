e35aec7fbc96ef276b7f33a8932e920ecb9e68fd
Jonathan Warykowski <jonathan@myzeller.com>
2026-07-08 21:58:13 +1000

    chore: updated mini configuration

--- common/.config/nvim/lua/plugins/mini.lua
+++ common/.config/nvim/lua/plugins/mini.lua
@@ -1,9 +1,51 @@
 return {
-	{ "echasnovski/mini.ai",          version = false, config = function() require("mini.ai").setup() end },
-	{ "echasnovski/mini.diff",        version = false, config = function() require("mini.diff").setup() end },
-	{ "echasnovski/mini.indentscope", version = false, config = function() require("mini.indentscope").setup() end },
-	{ "echasnovski/mini.operators",   version = false, config = function() require("mini.operators").setup() end },
-	{ "echasnovski/mini.pairs",       version = false, config = function() require("mini.pairs").setup() end },
-	{ "echasnovski/mini.statusline",  version = false, config = function() require("mini.statusline").setup() end },
-	{ "echasnovski/mini.surround",    version = false, config = function() require("mini.surround").setup() end },
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		config = function()
+			require("mini.ai").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.diff",
+		version = false,
+		config = function()
+			require("mini.diff").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		config = function()
+			require("mini.indentscope").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.operators",
+		version = false,
+		config = function()
+			require("mini.operators").setup({ replace = { prefix = "gR" } })
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		version = false,
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.statusline",
+		version = false,
+		config = function()
+			require("mini.statusline").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
 }
